6c21c4fc5574c70361fb3b71f57fedf867a700a7
Joydeep2005Banik <joydeepbanik41@gmail.com>
2026-08-20 14:11:33 +0530

    Add function to distribute elements into two arrays based on comparison

---
+++ distribute_elements_in_2arr1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>result={};
+        vector<int>v2={};
+        result.push_back(nums[0]);
+        v2.push_back(nums[1]);
+        for(int i=2;i<n;i++)
+        {
+            if(result.back()>v2.back())
+                result.push_back(nums[i]);
+            else
+                v2.push_back(nums[i]);
+            
+        }
+        result.insert(result.end(),v2.begin(),v2.end());
+        return result;
+    }
+};
