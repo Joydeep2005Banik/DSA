b44f5ca00aaf7e3f840338dd0aae21b27385d78d
Joydeep2005Banik <joydeepbanik41@gmail.com>
2026-09-05 13:06:37 +0530

    Add detailed comments explaining the logic for finding the smallest stable integer

--- STRIVER_A2Z/arrays/easy/smallestStable1.cpp
+++ STRIVER_A2Z/arrays/easy/smallestStable1.cpp
@@ -1,6 +1,18 @@
 // leetcode question = 3903
 
+/*
+INITIAL INTUITION:-
 
+-> we can see that the minimum suffix is dynamic i.e. for every max element the min element changes
+-> thus we iterate an outer loop from 0 to n in which we find the max element of nums for every iteration
+-> we initialise the variable to store the min element early inside the outer loop (comes in handy later)
+-> we run an inner loop from i to n in which we find the min element of the remaining subarray for that particular max value
+-> inside the outer loop we calculate the instability and check if it is <=k
+-> if true we find the minimum stability since there can be multiple stability<=k
+-> during the next outer loop iteration the min element variable gets re-initialised to be used only for the next max variable
+-> if the min. stability=INT_MAX we know that the stability condition wasnt matched and we return -1
+-> else we return stability value
+*/
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -31,3 +43,5 @@ int smallestStableInteger(vector<int>&nums, int k)
         return stable;
 }
 
+// TIME COMPLEXITY = O(N^2)
+// SPACE COMPLEXITY = O(1)
