72c9120479d2fd6d0bab34f0cef42a0d359b01d1
Joydeep2005Banik <joydeepbanik41@gmail.com>
2026-09-05 13:11:57 +0530

    Add detailed comments explaining the initial intuition and optimal approach for finding the first stable index

--- STRIVER_A2Z/arrays/medium/smallestStableIndex2.cpp
+++ STRIVER_A2Z/arrays/medium/smallestStableIndex2.cpp
@@ -1,5 +1,31 @@
 // leetcode question number = 3904
 
+/*
+INITIAL INTUITION:-
+
+-> we can see that the minimum suffix is dynamic i.e. for every max element the min element changes
+-> thus we iterate an outer loop from 0 to n in which we find the max element of nums for every iteration
+-> we initialise the variable to store the min element early inside the outer loop (comes in handy later)
+-> we run an inner loop from i to n in which we find the min element of the remaining subarray for that particular max value
+-> inside the outer loop we calculate the instability and check if it is <=k
+-> if true we find the minimum stability since there can be multiple stability<=k
+-> during the next outer loop iteration the min element variable gets re-initialised to be used only for the next max variable
+-> if the min. stability=INT_MAX we know that the stability condition wasnt matched and we return -1
+-> else we return stability value
+
+TIME COMPLEXITY = O(N^2)
+SPACE COMPLEXITY=O(N)
+*/
+
+/*
+OPTIMAL APPROACH:-
+
+-> we use a vector for the min suffix to store the min values from n-2 to 0 and nums[n-1] is guaranteed to be a min value
+-> in a seperate iteration run we find the max element of the prefix side
+-> still inside the iteration run only, we calculate the instability and check <=k
+-> if true we immediately return the index since the first index is guaranteed to be the minimum index position
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
